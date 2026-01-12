f414d1af0202e9acbd2588f15a370a69818dced8
intakhabchy <intakhab.chy@gmail.com>
2026-01-13 03:05:45 +0600

    footer changed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="bg-light text-center py-2 mt-auto shadow-sm">
-    &copy; 2026 Expense Note
+    &copy; 2026 Expense Note. All rights reserved.
   </footer>
 </template>
